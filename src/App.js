d67212bb51eaee178b44a8a416f72f501330ded3
codessi <johansuh@gmail.com>
2022-03-11 11:39:55 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@
 import './App.scss';
 import illustration from "./../src/images/illustration-woman-online-mobile.svg"
 import illustrationDesk from "./../src/images/illustration-woman-online-desktop.svg"
+import box from "./../src/images/illustration-box-desktop.svg"
 
 import bigPattern from "./../src/images/bg-pattern-mobile.svg"
+import bigPatternDesk from "./../src/images/bg-pattern-desktop.svg"
 
 import Question from './Question';
 import data from './data';
@@ -15,10 +17,17 @@ function App() {
       <div className="card">
         <div className="img-container">
           <picture>
+            <source media="(min-width:900px)" srcset={bigPatternDesk}/>
+            <img className= "pattern" src={bigPattern}alt="" />
+          </picture>
+          <picture>
             <source media="(min-width:900px)" srcset={illustrationDesk}/>
             <img className= "hero" src={illustration} alt="" />
           </picture>
-          <img className= "pattern" src={bigPattern}alt="" />
+          <picture>
+            <source media="(min-width:900px)" srcset={box}/>
+            <img className= "box" src=""alt="" />
+          </picture>
         </div>
         <div className="content-container">
           <h3 className="title">FAQ</h3>
@@ -36,7 +45,11 @@ function App() {
             <div className={show? 'show': 'hide'}>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</div>
           </article> */}
         </div>
-      </div>
+      </div>  {/* card */}
+      {/* <picture>
+            <source media="(min-width:900px)" srcset={box}/>
+            <img className= "box-two" src=""alt="" />
+      </picture> */}
     </>
   );
 }
