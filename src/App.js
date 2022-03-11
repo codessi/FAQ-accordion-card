58119923ac7bdfb5a3b4a9ef7e0dbb3f5d618343
codessi <johansuh@gmail.com>
2022-03-11 11:44:48 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -36,20 +36,9 @@ function App() {
       return(       
          <Question key={index} question={question } answer={answer} />   )
     })}
-    
-          {/* <article>
-            <div className="center">
-              <div className={`questions ${show&&"bold"}`}>How many team members can I invite?</div>
-              <button onClick={handleClick}><img src={arrow} className={!show ?"arrow":"down-arrow"} alt="" /></button>
-            </div>
-            <div className={show? 'show': 'hide'}>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</div>
-          </article> */}
+
         </div>
       </div>  {/* card */}
-      {/* <picture>
-            <source media="(min-width:900px)" srcset={box}/>
-            <img className= "box-two" src=""alt="" />
-      </picture> */}
     </>
   );
 }
